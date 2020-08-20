f1b5a4c6621be76fed86cd165e748b227a9e86d3
Sean Wilson <paintballsean@gmail.com>
2020-08-20 11:43:57 -0700

    Commit adds part of function i am trying.
    Co-authored-by: Kolman Soifer <kolmansoifer@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,38 @@
 $(document).ready(function() {
 
-  $("form#").submit(function(event) {
+  $("form#roman").submit(function(event) {
     event.preventDefault();
+	
+  	for intergerRoman(){
+		let romanNumeral = "";
+		const romanNumeralList = {M:1000, D:500, C:100,L:50, X:10, V:5, I:1};
 
-    
+		if(romanNumeral < 1 || rumanNumeral)
+    	return "Sorry but Roman Numerals are only 1 to 3999 please reenter!"
+
+}
   });
 });
+
+/*
+Business Logic:
+
+Describe: romanNumerals()
+Test: The program will add the value of all the symbols.
+
+
+Test: Program will limit # of characters to <= 3.
+Expect
+
+Test: Program will swich to subtraction if # of characters is > 3.
+Expect
+
+Test: Program will separate ones, tens, hundreds and thousands.
+Expect
+
+Test: Program will limit # of characters to <= 3999.
+Expect
+
+
+
+*/
